99cabf1e3ff5d00f33fd4a41292ebc98b91b8b6d
kratika19 <56781757+kratika19@users.noreply.github.com>
2020-04-07 01:23:00 +0530

    Add files via upload

---
+++ ORANGE.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 14:31:38 2020
+
+@author: krati
+"""
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, img = cap.read()
+    hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    
+    lower_orange = np.array([0,0,255])
+    upper_orange = np.array([97,105,255])
+    mask = cv2.inRange(hsv , lower_orange , upper_orange)
+    
+     
+    
+    blur=cv2.GaussianBlur(mask,(5,5),0)
+    
+    
+    contours, hierarchy = cv2.findContours(blur,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+    for cnt in contours:
+        area=0
+        area=cv2.contourArea(cnt)
+        if area>500:
+            x,y,m,h=cv2.boundingRect(cnt)
+            img= cv2.rectangle(img,(x,y),(x+m,y+h),(255,0,0),2)
+            
+            
+    cv2.imshow("mask",blur)
+    cv2.imshow("frame",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break     
+
+cv2.destroyAllWindows()
